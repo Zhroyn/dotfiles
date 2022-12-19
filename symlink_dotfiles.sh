a3da015e53314c5c216e263d9b9ba6c629652fa1
Pxtkn <hr.zheng@outlook.com>
2022-12-19 19:29:36 +0800

    modify symlink_dotfiles.sh (cancel backup function)

--- symlink_dotfiles.sh
+++ symlink_dotfiles.sh
@@ -1,32 +1,11 @@
 #!/usr/bin/env bash
 
-# mkdir with the first argument. If the directory already exists, append _
-# This function is used to create backup folder
-function makedir() {
-    if [ -d $1 ]; then
-        param="$1_"
-        echo $(makedir $param)
-    else
-        mkdir $1
-        echo $1
-    fi
-}
-
-# Create backup folder. It should not have the same name as any existing one.
-backupdir=$(makedir "dotfiles_backup")
-
-# If the backup folder is used then at the end of the script, this variable
-# will have been changed. Otherwise we can assume that the folder hasn't
-# been used and can be safely removed.
-backedup=0
-
 # Ignore certain git files and folders
 ignores=(
         .git
         .gitignore
         .gitmodules
 )
-
 
 # Do stuff to each file starting with '.' in ~/dotfiles/
 for file in .[!.]*; do
@@ -37,32 +16,12 @@ for file in .[!.]*; do
     for ig in ${ignores[*]}; do
         if [ "$ig" == "$file" ]; then
             skip=1
-            echo "Ignored $file"
+	    echo "Ignored $file"
             break
         fi
     done
     if [ $skip == 1 ]; then
         continue
-    fi
-
-    # If dot file already exists in ~
-    if [ -e ~/$file ]; then
-        # TODO: sometimes the existing dot file is actually a symlink that
-        # points to another dot file. This script may have broken such
-        # symlinks by moving the target dot file. If that's the case then
-        # this IF will return false, and you'll have to manually
-        # create your symlink again when you want to restore your settings.
-        # I've yet to find a solution for this but hopefully someday I will.
-
-        # Make sure the script does not remove the backup file, now that it is
-        # actually used
-        backedup=1
-
-        # Move file into backup folder
-        mv ~/$file ~/$backupdir/$file
-
-        echo "Backed up ~/$file to ~/$backupdir/$file"
-
     fi
 
     # Create symlink for files inside ~/dotfiles/
@@ -72,9 +31,5 @@ for file in .[!.]*; do
 
 done
 
-# Remove backup dir only if it hasn't been used
-if [ "$backedup" -eq 0 ]; then
-    rm -rf $backupdir
-fi
-
+# Implement the config
 source ~/.bashrc
